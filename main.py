061c3dbdac5fed9ecb0aa4d58b18b638e255e144
david <david@itarverne.fr>
2025-10-07 14:17:51 +0200

    feat: type Sequence to require invariant or covariant parameter

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from typing import Sequence
+
 class Animal:
   pass
 
-class Dog(Animal):
+class Dog(Animal):  
   pass
 
-def feed_animals(animals: list[Animal]):
+def feed_animals(animals: Sequence[Animal]):
   for animal in animals:
     print(f"Feeding a(n) {type(animal).__name__}")
 
-dogs = [Dog()]
-feed_animals(dogs) # Mypy: Argument 1 to "feed_animals" has incompatible type "list[Dog]"; expected "list[Animal]" [arg-type]
+dogs = [Dog(), Dog()]
+feed_animals(dogs) # Ceci est valide
