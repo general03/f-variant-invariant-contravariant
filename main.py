9bb72bfcf3ccc64539cbb083bd2d5b28854fb3d6
david <david@itarverne.fr>
2025-10-13 11:51:33 +0200

    feat: type generique in python < 3.12

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from typing import Final
+from typing import Generic, TypeVar
 
+class Dog:
+    pass
 
-class Animal:
-  pass
+T_co = TypeVar('T_co', covariant=True)
+class Box(Generic[T_co]):
+    def __init__(self, content: T_co) -> None:
+        self._content = content
 
-class Dog(Animal):
-  pass
+def do_something(dog: Box[Dog]):
+    pass
 
-class Box[T]:
-    def __init__(self, content: T) -> None:
-        self.content : Final = content # public attribut
-
-def play_with_dog(box: Box[Animal]):
-    print(f"Play in {type(box).__name__}")
-
-box = Box(Dog()) # Box(Animal()) is also OK
-play_with_dog(box)
+do_something(Box(Dog()))
