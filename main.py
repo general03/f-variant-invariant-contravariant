a335dac90be32a86943d0737264b646a7e721804
david <david@itarverne.fr>
2025-11-06 11:26:53 +0100

    feat: type generique T with method updated type infers
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ class Box[T]:
     def __init__(self, content: T) -> None:
         self._content = content
 
+    def set_content(self, content: T) -> T: # This method infers the generic type
+        self._content = content
+        return self._content
+
 def play_with_dog(box: Box[Animal]):
     print(f"Play in {type(box).__name__}")
 
-box = Box(Dog()) # Box(Animal()) is also OK
-play_with_dog(box)
+box = Box(Dog())
+play_with_dog(box) # Mypy: Argument 1 to "play_with_dog" has incompatible type "Box[Dog]"; expected "Box[Animal]"
